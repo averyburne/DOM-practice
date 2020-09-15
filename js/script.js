938b6d65b6621ae1393fbfa950a6c84b5401ced0
Avery Burne <burneav@gmail.com>
2020-09-15 18:26:44 -0400

    Linking files seems to partially work for js
    file will load, but will not have access to html since it hasn't loaded
    adding defer to script tag makes it wait till all has loaded
    can't get event to load only when a button is clicked

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("paragraphButton").onClick = changeParagraph
+
+function changeParagraph() {
+  document.getElementById("second").innerHTML = "This is that new new"
+  console.log('peen')
+}
